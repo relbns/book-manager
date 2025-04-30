b80468b6810ea27a1fc3efcb2fe01ab93fc9c420
relbns <ariel@stoketalent.com>
2025-04-30 18:02:10 +0300

    Updates

---
+++ assets/DeleteOutlined-Ntdf4IE3.js
@@ -0,0 +1,2 @@
+import{r as o,I as s}from"./index-ColAZZak.js";import{D as c}from"./DeleteOutlined-DFutYCzo.js";function a(){return a=Object.assign?Object.assign.bind():function(e){for(var t=1;t<arguments.length;t++){var n=arguments[t];for(var r in n)Object.prototype.hasOwnProperty.call(n,r)&&(e[r]=n[r])}return e},a.apply(this,arguments)}const i=(e,t)=>o.createElement(s,a({},e,{ref:t,icon:c})),p=o.forwardRef(i);export{p as R};
+//# sourceMappingURL=DeleteOutlined-Ntdf4IE3.js.map
