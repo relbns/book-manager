f21ccc78e3e9386d7477f76ca0f1c62b13f0698a
relbns <ariel@stoketalent.com>
2025-04-30 18:18:24 +0300

    Updates

---
+++ assets/ResponsiveFilterContainer-CN9EqLKP.js
@@ -0,0 +1,16 @@
+import{j as i,g as a}from"./index-DSzfMtvJ.js";const r=a.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 16px;
+  margin-bottom: 24px;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: stretch;
+
+    & > * {
+      width: 100%;
+    }
+  }
+`,s=({children:t,className:e=""})=>i.jsx(r,{className:`filter-container ${e}`,children:t});export{s as R};
+//# sourceMappingURL=ResponsiveFilterContainer-CN9EqLKP.js.map
