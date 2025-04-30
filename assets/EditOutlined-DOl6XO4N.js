51959668394c5f18502f125e07e749522935ef57
relbns <ariel@stoketalent.com>
2025-04-30 18:12:29 +0300

    Updates

--- assets/EditOutlined-DOl6XO4N.js
+++
@@ -1,2 +0,0 @@
-import{r as o,I as s,aT as i}from"./index-C0-qEfuO.js";function e(){return e=Object.assign?Object.assign.bind():function(n){for(var t=1;t<arguments.length;t++){var r=arguments[t];for(var a in r)Object.prototype.hasOwnProperty.call(r,a)&&(n[a]=r[a])}return n},e.apply(this,arguments)}const c=(n,t)=>o.createElement(s,e({},n,{ref:t,icon:i})),p=o.forwardRef(c);export{p as R};
-//# sourceMappingURL=EditOutlined-DOl6XO4N.js.map
